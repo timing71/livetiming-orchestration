da45515bc0497dcb09aacecdf22e6bda8cef6209
James Muscat <jamesremuscat@gmail.com>
2020-05-08 12:16:54 +0100

    Update setup.py to be orchestration-specific.

--- setup.py
+++ setup.py
@@ -2,36 +2,34 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='livetiming-core',
-    description='Timing71 live timing aggregator - core functionality',
+    name='livetiming-orchestration',
+    description='Timing71 live timing aggregator - orchestration functionality',
     author='James Muscat',
     author_email='jamesremuscat@gmail.com',
-    url='https://github.com/jamesremuscat/livetiming',
+    url='https://github.com/timing71/livetiming-orchestration',
     packages=find_packages('src', exclude=["*.tests"]),
     package_dir={'': 'src'},
     long_description='''
-    Core functionality for the Timing 71 live timing aggregator.
+    Orchestration functionality for the Timing 71 live timing aggregator.
 
-    This is a framework for obtaining, processing, analysing and publishing
-    motorsport live timing data feeds from a variety of sources.
+    This includes the scheduler, service manager, DVR, directory, and
+    schedule-assistance tooling.
     ''',
     install_requires=[
         "autobahn[serialization,twisted]>=17.6.2",
         "dictdiffer",
         "google-api-python-client",
         "icalendar",
+        "livetiming-core",
         "lzstring==1.0.3",
         "oauth2client",  # Not included in google-api-python-client despite what Google say
-        "pluginbase",
         "pyopenssl",
-        "python-dateutil",
         "python-dotenv",
         "python-twitter",
         "sentry-sdk",
         "service_identity",
         "simplejson",
         "subprocess32",
-        "treq",
         "twisted"
     ],
     setup_requires=[
@@ -44,15 +42,11 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'livetiming-analysis = livetiming.generate_analysis:main',
             'livetiming-directory = livetiming.orchestration.directory:main',
             'livetiming-dvr = livetiming.orchestration.dvr:main',
-            'livetiming-recordings = livetiming.recording:main',
-            'livetiming-recordings-index = livetiming.recording:update_recordings_index',
             'livetiming-rectool = livetiming.orchestration.rectool:main',
             'livetiming-schedule = livetiming.orchestration.schedule.__main__:main',
             'livetiming-scheduler = livetiming.orchestration.scheduler:main',
-            'livetiming-service = livetiming.service:main',
             'livetiming-service-manager = livetiming.orchestration.servicemanager:main',
         ],
     }
